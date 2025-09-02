637122b21afba79abffc4296fa833e06a017d4af
linkin <pablogermanjimenez7@gmail.com>
2025-09-02 12:56:51 -0300

    bagheera reloca

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,19 +7,39 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Image from "react-bootstrap/Image";
 import Col from "react-bootstrap/Col";
-import SalemQuebrao from '../assets/salemquebrao.bmp'
-import DoctoresPet from '../assets/doctorespets.bmp'
-import SalemPolenta from '../assets/salempolenta.bmp'
-import BagheeraBella from '../assets/bagheerabella.bmp'
+import SalemQuebrao from "../assets/salemquebrao.bmp";
+import DoctoresPet from "../assets/doctorespets.bmp";
+import SalemPolenta from "../assets/salempolenta.bmp";
+import BagheeraBella from "../assets/bagheerabella.bmp";
+import { useForm } from "react-hook-form";
 
 function FormCitas() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  
+  const postValidation = (data) => {
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    console.log(data);
+  };
+
   return (
     <>
-      <Navbar expand="lg" className="d-flex shadow-lg p-3 mb-5 bg-body rounded border border-3 border-success-subtle">
+      <Navbar
+        expand="lg"
+        className="d-flex shadow-lg p-3 mb-5 bg-body rounded border border-3 border-success-subtle"
+      >
         <Container>
-          <Navbar.Brand  href="#home">🐈‍⬛Veterinaria Bagheera🐾</Navbar.Brand>
+          <Navbar.Brand href="#home">🐈‍⬛Veterinaria Bagheera🐾</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -35,9 +55,7 @@ function FormCitas() {
                 <NavDropdown.Item href="#action/3.2">
                   Gorrito cheto
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Snacks
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Snacks</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
                   Juguetitos
@@ -49,52 +67,101 @@ function FormCitas() {
       </Navbar>
       <Card className="text-center container mt-4 rounded-4 shadow-lg p-3 mb-5 bg-body rounded border border-3 border-success-subtle">
         <Card.Body>
-          <Card.Title>🐶Veterinaria Online: Atención profesional para tu mascota, estes donde estés!🐱🐾</Card.Title>
+          <Card.Title>
+            🐶Veterinaria Online: Atención profesional para tu mascota, estes
+            donde estés!🐱🐾
+          </Card.Title>
           <Card.Subtitle className="mb-2 b-2 text-success">
             📲Consulta Veterinaria online 24/7💕
           </Card.Subtitle>
           <Card.Text className="fs-5 text-danger">
-            Sin salir de casa, sin estrés para tu mascota, con resultados inmediatos.
+            Sin salir de casa, sin estrés para tu mascota, con resultados
+            inmediatos.
           </Card.Text>
           <div className="d-flex justify-content-center obj-align-center  flex-wrap gap-3 ">
-         <Col xs={8} md={4}>
-         <Image id="bagheera-bella" src={BagheeraBella} roundedCircle/>
-         </Col>
-         <Col xs={8} md={4}>
-          <Image id="salem-polenta" src={SalemPolenta} roundedCircle />
-        </Col>
-         <Col xs={8} md={4}>
-          <Image  id="DrPichichu" src={DoctoresPet} roundedCircle  />
-        </Col>
-          <Col xs={8} md={4}>
-          <Image id="salem-quebrao" src={SalemQuebrao} roundedCircle />
-        </Col>
+            <Col xs={8} md={4}>
+              <Image id="bagheera-bella" src={BagheeraBella} roundedCircle />
+            </Col>
+            <Col xs={8} md={4}>
+              <Image id="salem-polenta" src={SalemPolenta} roundedCircle />
+            </Col>
+            <Col xs={8} md={4}>
+              <Image id="DrPichichu" src={DoctoresPet} roundedCircle />
+            </Col>
+            <Col xs={8} md={4}>
+              <Image id="salem-quebrao" src={SalemQuebrao} roundedCircle />
+            </Col>
           </div>
         </Card.Body>
       </Card>
-      <Form className="container shadow-lg p-3 mb-5 bg-body rounded-3 border border-3 border-success-subtle">
+      <Form
+        className="container shadow-lg p-3 mb-5 bg-body rounded-3 border border-3 border-success-subtle"
+        onClick={handleSubmit(postValidation)}
+      >
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Tu Email </Form.Label>
-          <Form.Control type="email" placeholder="Tu Email!" />
+          <Form.Control
+            type="email"
+            placeholder="Tu Email!"
+            {...register("email", { required: true, maxLength: 20 })}
+          />
+          {errors.email && <p className="text-danger">Campo obligatorio!</p>}
         </Form.Group>
         <Form.Group className="mb-3" controlId="celNumber">
           <Form.Label>Cel para contactarte</Form.Label>
-          <Form.Control type="number" placeholder="Ingresá tu numero de cel!" />
+          <Form.Control
+            type="number"
+            placeholder="Ingresá tu numero de cel!"
+            {...register("number", {
+              required: true,
+              minLength: 10,
+              maxLength: 10,
+              pattern: "d/g",
+            })}
+          />
+          {errors.number && (
+            <p className="text-danger">Campo obligatorio, 10 digitos max!</p>
+          )}
         </Form.Group>
-        
+
         <Form.Group className="mb-3" controlId="celNumber">
           <Form.Label>Dirección</Form.Label>
-          <Form.Control type="number" placeholder="Iremos en caso de ser necesario" />
+          <Form.Control
+            type="text"
+            placeholder="Iremos en caso de ser necesario"
+            {...register("text", {
+              requeried: true,
+              maxLength: 50,
+              pattern: "d[aeiou]/g",
+            })}
+          />
+          {errors.text && (
+            <p className="text-danger">Campo obligatorio perrin!</p>
+          )}
         </Form.Group>
-        
-        <Form.Group className="mb-3" controlId="celNumber">
+
+        <Form.Group className="mb-3" controlId="sintomasPichichu">
           <Form.Label>Edad,peso,síntomas</Form.Label>
-          <Form.Control type="number" placeholder="Excluyentes, ingresar estimativos" />
+          <Form.Control
+            type="number"
+            placeholder="Excluyentes, ingresar estimativos"
+          />
         </Form.Group>
-        
+
         <Form.Group className="mb-3" controlId="celNumber">
           <Form.Label>Cel para contactarte</Form.Label>
-          <Form.Control type="number" placeholder="Ingresá tu numero de cel!" />
+          <Form.Control
+            type="number"
+            placeholder="Ingresá tu numero de cel!"
+            {...register("number", {
+              required: true,
+              minLength: 5,
+              maxLength: 10,
+            })}
+          />
+          {errors.number && (
+            <p className="text-danger">Campo obligatorio, 10 digitos max!</p>
+          )}
         </Form.Group>
         <Button variant="primary" type="submit" className="mb-3">
           Submit
